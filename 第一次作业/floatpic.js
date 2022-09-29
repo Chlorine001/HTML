19454b5df2f31ca14af35ad73ecbb49205e2d914
Chlorine001 <1151259938@qq.com>
2022-09-29 17:47:40 +0800

    first modification

---
+++ 第一次作业/floatpic.js
@@ -0,0 +1,45 @@
+﻿var xin = true,
+    yin = true;
+var step = 1;
+var delay = 15;
+var $obj;
+$(function() {
+    $obj = $("#imgDiv");
+	var img_left = $(window).width() - $obj.width(); // 右边界
+	$obj.css("left",img_left);
+    var time = window.setInterval("move()", delay);
+    $obj.mouseover(function() {
+        clearInterval(time)
+    });
+    $obj.mouseout(function() {
+        time = window.setInterval("move()", delay)
+    });
+});
+
+function move() {
+    var left = $obj.offset().left;
+    var top = $obj.offset().top;
+    var L = T = 0; //左边界和顶部边界
+    var R = $(window).width() - $obj.width(); // 右边界
+    var B = $(window).height() - $obj.height(); //下边界
+    if (left < L) {
+        xin = true; // 水平向右移动
+    }
+    if (left > R) {
+        xin = false;
+    }
+    if (top < T) {
+        yin = true;
+    }
+    if (top > B) {
+        yin = false;
+    }
+    left = left + step * (xin == true ? 1 : -1);
+    top = top + step * (yin == true ? 1 : -1);
+    // 给div 元素重新定位
+    $obj.offset({
+        top: top,
+        left: left
+    })
+}
+$("#imgDiv").draggable();
